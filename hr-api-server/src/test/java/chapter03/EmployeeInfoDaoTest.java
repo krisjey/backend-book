0cc2d0a13c736bc7fce9b1abbaa84ba91c89816c
krisjey <krisjey@afreecatv.com>
2017-09-08 19:08:24 +0900

    Add test case EmployeeInfoDaoTest

--- hr-api-server/src/test/java/chapter03/EmployeeInfoDaoTest.java
+++ hr-api-server/src/test/java/chapter03/EmployeeInfoDaoTest.java
@@ -1,25 +1,31 @@
 
 package chapter03;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.assertj.core.api.Assertions.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.sql.SQLException;
 
 import org.junit.Test;
 
 public class EmployeeInfoDaoTest {
-
   @Test
   public void test() {
     EmployeeInfoDao employeeInfoDao = new EmployeeInfoDao();
-    assertThat(employeeInfoDao).isNotNull();
+//    assertThat(employeeInfoDao).isNotNull();
+    assertNotNull(employeeInfoDao);
     try {
-      employeeInfoDao.getEmployeeInfoByNo("123123");
+      StringBuffer result = employeeInfoDao.getEmployeeInfoByNo("403144");
+//      assertThat(result).isNotNull();
+      assertNotNull(result);
+      assertEquals("403144|Kristin Serra|Staff|Sales|1993-02-27|57459\r\n",
+          result.toString());
     }
     catch (SQLException e) {
-      fail("");
+      fail("SQL 실행 오류");
     }
   }
-
 }
