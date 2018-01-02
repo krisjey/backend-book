51a441bb27e82dabd710316f2be02b003c87d141
krisjey <krisjey@kris-PC.office.afreecatv.com>
2018-01-02 21:42:51 +0900

    3장 코드 완료

---
+++ hr-api-server/src/main/java/chapter03/BasicDao.java
@@ -0,0 +1,57 @@
+package chapter03;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+public class BasicDao {
+  private Logger logger = Logger.getLogger(BasicDao.class);
+  
+  protected Connection getConnection() {
+    // FIXME serverTimezone=UTC Àû¿ë
+    String url = "jdbc:mysql://172.28.128.3:3306/employees?serverTimezone=UTC";
+    String username = "backend";
+    String password = "kris34#$";
+
+    Connection connect = null;
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+      Properties info = new Properties();
+      info.setProperty("user", username);
+      info.setProperty("password", password);
+      info.setProperty("characterSetResults", "utf-8");
+
+      connect = DriverManager.getConnection(url, info);
+    }
+    catch (ClassNotFoundException e) {
+      logger.error(e.getMessage(), e);
+    }
+    catch (SQLException e) {
+      logger.error(e.getMessage(), e);
+    }
+
+    return connect;
+  }
+  
+  protected StringBuffer toApiResult(ResultSet resultSet) throws SQLException {
+    StringBuffer builder = new StringBuffer();
+
+    if (resultSet != null) {
+      while (resultSet.next()) {
+        for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+          if (i != 1) {
+            builder.append("|");
+          }
+          builder.append(resultSet.getString(i));
+        }
+        builder.append("\r\n");
+      }
+    }
+
+    return builder;
+  }
+}
